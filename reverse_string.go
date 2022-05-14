bb6a32fecbd3b9077bf88e2882fea5abb354e2d6
Gogi <Gogi22@users.noreply.github.com>
2022-05-14 09:31:46 +0400

    reversed a string

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,16 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	temp := make([]rune, 0)
+
+	for _, v := range input {
+		temp = append(temp, v)
+	}
+
+	for i := 0; i < len(temp)/2; i++ {
+		temp[i], temp[len(temp)-i-1] = temp[len(temp)-i-1], temp[i]
+	}
+
+	output = string(temp)
+	return
 }
